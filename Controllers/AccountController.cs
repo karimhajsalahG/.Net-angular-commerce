e3acbec405f8ece0f603c9afbcb2c291e996a625
karimhajsalahG <karimhajsalah056@gmail.com>
2021-10-10 18:21:16 +0200

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using AngularToApiw.Models;
+using AngularToApiw.ModelViews;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AngularToApiw.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ApplicationDB _db;
+        private readonly UserManager<ApplicationUser> _manager;
+
+        public AccountController(ApplicationDB db, UserManager<ApplicationUser> manage)
+        {
+            _db = db;
+            _manager = manage;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (model == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                if (EmailExistes(model.Email))
+                {
+                    return BadRequest("Email is used");
+                }
+                if (!IsEmailValid(model.Email))
+                {
+                    return BadRequest("Email not valid!!");
+                }
+                if (UserNameExistes(model.UserName))
+                {
+                    return BadRequest("UserName is used");
+                }
+                var user = new ApplicationUser
+                {
+                    Email = model.Email,
+                    UserName = model.UserName
+
+                };
+                // saved in data base
+                var result = await _manager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status200OK);
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+
+            }
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+
+        private bool UserNameExistes(string userName)
+        {
+            return _db.Users.Any(x => x.UserName == userName);
+        }
+
+        private bool EmailExistes(string email)
+        {
+            return _db.Users.Any(x => x.Email == email);
+        }
+        private bool IsEmailValid(string email)
+        {
+            Regex em = new Regex(@"\w+\@\w+.com|\w+\@\w+.net");
+            if (em.IsMatch(email))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,67 @@
+using AngularToApiw.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AngularToApiw
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<ApplicationDB>(option => option.UseSqlServer(Configuration.GetConnectionString("MyConnection")));
+            services.AddIdentity<ApplicationUser, ApplicationRole>(option=> {
+
+                //conditon passss
+                option.Password.RequireDigit = true;
+                option.Password.RequireLowercase = true;
+                option.Password.RequiredLength = 6;
+                option.Password.RequiredUniqueChars = 1;
+                option.Password.RequireUppercase = true;
+                option.Password.RequireNonAlphanumeric = true;
+            }).AddEntityFrameworkStores<ApplicationDB>();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
